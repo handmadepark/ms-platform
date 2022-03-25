4358b3e491c451ae318fd3f585c5c4efcee39ef9
handmadepark950 <handmadepark950@gmail.com>
2022-03-25 13:02:45 +0400

    login commit

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,1 +1,16 @@
-This is admin login page
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin login</title>
+</head>
+<body>
+    <form action="{{ route('admin.auth) }}" method="post">
+        <input type="email" name="password">
+        <input type="password" name="password">
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
