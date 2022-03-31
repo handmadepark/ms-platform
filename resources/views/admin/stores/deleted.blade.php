67154b31ad2b42b406a3283caad90aa21e032f1c
handmadepark950 <handmadepark950@gmail.com>
2022-03-31 18:31:01 +0400

    31.03.2022

---
+++ resources/views/admin/stores/deleted.blade.php
@@ -0,0 +1,71 @@
+@extends('admin.layouts.master')
+@section('content')
+    <div class="main-content app-content mt-5">
+        <div class="main-container container-fluid">
+            <div class="row row-sm">
+                <div class="col-xl-12">
+                    <div class="card">
+                        <div class="card-header pb-0">
+                            <div class="d-flex justify-content-between">
+                                <h4 class="card-title mg-b-0">Deleted countries</h4>
+                                <div class="float-end">
+                                    <a href="{{ URL::previous() }}">
+                                        <button class="btn btn-sm btn-danger">
+                                            <span><i class="fas fa-arrow-left"></i></span>
+                                            Back
+                                        </button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-hover mb-0 text-md-nowrap text-center">
+                                    <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Status</th>
+                                        <th>Operations</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @forelse($countries as $country)
+                                        <tr>
+                                            <td>{{$country->id}}</td>
+                                            <td>{{$country->name}}</td>
+                                            <td>
+                                                @if($country->status==1)
+                                                    <button class="btn btn-sm btn-outline-success">Active</button>
+                                                @else
+                                                    <button class="btn btn-sm btn-outline-warning">Inactive</button>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                <a href="{{ route('admin.countries.restore', ['id'=>$country->id])  }}">
+                                                    <button class="btn btn-sm btn-info">
+                                                        <span><i class="fas fa-redo-alt"></i></span>
+                                                    </button>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="4">
+                                                <p class="text-danger text-center">There is no deleted country</p>
+                                            </td>
+                                        </tr>
+                                    @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+@endsection
---
+++ resources/views/admin/stores/edit.blade.php
@@ -0,0 +1,44 @@
+@extends('admin.layouts.master')
+@section('content')
+    <div class="main-content app-content mt-5">
+        <div class="main-container container-fluid">
+            <div class="row row-sm">
+                <div class="col-xl-12">
+                    <div class="card">
+                        <div class="card-header pb-0">
+                            <div class="d-flex justify-content-between">
+                                <h4 class="card-title mg-b-0">{{$item_selected->name}}</h4>
+                                <div class="float-end">
+                                    <a href="{{ URL::previous() }}">
+                                        <button class="btn btn-sm btn-danger">
+                                            <span><i class="fas fa-arrow-left"></i></span>
+                                            Back
+                                        </button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{route('admin.countries.update', ['id'=>$item_selected->id])}}" method="POST">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="name">Country name</label>
+                                    <input type="text" class="form-control" name="name" id="name" aria-describedby="emailHelp" value="{{$item_selected->name}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="status">Status</label>
+                                    <select name="status" id="status" class="form-control">
+                                        <option disabled selected>Please select</option>
+                                        <option value="0" {{ $item_selected->status==0 ? 'selected' : ''  }}>Deactive</option>
+                                        <option value="1" {{ $item_selected->status==1 ? 'selected' : ''  }}>Active</option>
+                                    </select>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
